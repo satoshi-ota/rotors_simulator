5cab4e0ced0ad27ec1f6aae3106723a575396878
satoshiota <satoshi.ota928@gmail.com>
2019-10-04 17:24:36 +0900

    version hongo_aero

--- rotors_joy_interface/src/joy.cpp
+++ rotors_joy_interface/src/joy.cpp
@@ -39,7 +39,7 @@ Joy::Joy() {
 
   pnh.param("axis_roll_", axes_.roll, 0);
   pnh.param("axis_pitch_", axes_.pitch, 1);
-  pnh.param("axis_thrust_", axes_.thrust, 2);
+  pnh.param("axis_thrust_", axes_.thrust, 4);
 
   pnh.param("axis_direction_roll", axes_.roll_direction, -1);
   pnh.param("axis_direction_pitch", axes_.pitch_direction, 1);
@@ -55,9 +55,9 @@ Joy::Joy() {
 
   pnh.param("is_fixed_wing", is_fixed_wing_, false);
 
-  pnh.param("button_yaw_left_", buttons_.yaw_left, 3);
-  pnh.param("button_yaw_right_", buttons_.yaw_right, 4);
-  pnh.param("button_ctrl_enable_", buttons_.ctrl_enable, 5);
+  pnh.param("button_yaw_left_", buttons_.yaw_left, 4);
+  pnh.param("button_yaw_right_", buttons_.yaw_right, 5);
+  pnh.param("button_ctrl_enable_", buttons_.ctrl_enable, 6);
   pnh.param("button_ctrl_mode_", buttons_.ctrl_mode, 10);
   pnh.param("button_takeoff_", buttons_.takeoff, 7);
   pnh.param("button_land_", buttons_.land, 8);
